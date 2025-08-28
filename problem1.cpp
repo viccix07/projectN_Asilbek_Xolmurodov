ac7fb45e693137bc31e76cde804a05dee4ee0519
viccix7 <asilbekx777@gmail.com>
2025-08-28 12:40:04 +0500

    commit

--- problem1.cpp
+++ problem1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    cout <<"Hello, NUU"<< endl;
+    cout <<"Hello, NUU!"<< endl;
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
